162a38a8af95520f5e88c7f8136de1eb1b208712
pakalalap <99343872+pakalalap@users.noreply.github.com>
2022-04-18 11:12:46 +0800

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -4,7 +4,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/angahpadu/ip/main/access.sh | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/angahpadu/ip/main/access | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e "${green} Please Wait, Proses...${NC}"
